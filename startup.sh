bca58062eeb5a922b1aa8c66dd69241a499ed1c7
5nine <h04maper@gmail.com>
2026-01-03 22:02:45 +0100

    attempt to hide translate prompt again

--- startup.sh
+++ startup.sh
@@ -26,14 +26,14 @@ fi
 
 # Start touch chromium page on DSI-1 screen (window-position=0,0)
 echo "Starting touch display..."
-/usr/bin/chromium --kiosk --password-store=basic --use-mock-keychain --disable-translate --user-data-dir=/tmp/chromium-touch --window-position=0,0 http://localhost:3000/ui/touch.html &
+/usr/bin/chromium --kiosk --password-store=basic --use-mock-keychain --disable-features=TranslateUI,Translate --lang=en-US --user-data-dir=/tmp/chromium-touch --window-position=0,0 http://localhost:3000/ui/touch.html &
 
 # Wait a moment before starting the second instance
 sleep 1
 
 # Start tv chromium page on HDMI-1-1 screen (window-position=1280,0)
 echo "Starting TV display..."
-/usr/bin/chromium --kiosk --password-store=basic --use-mock-keychain --disable-translate --user-data-dir=/tmp/chromium-tv --window-position=1280,0 http://localhost:3000/ui/tv.html &
+/usr/bin/chromium --kiosk --password-store=basic --use-mock-keychain --disable-features=TranslateUI,Translate --lang=en-US --user-data-dir=/tmp/chromium-tv --window-position=1280,0 http://localhost:3000/ui/tv.html &
 
 echo "Startup complete. Both displays should be active."
 

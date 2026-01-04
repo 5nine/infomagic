66b50d2dfee3ca8fb642eb2b0809fdfe44057259
5nine <h04maper@gmail.com>
2026-01-04 02:45:42 +0100

    added reboot to sudoers

--- install.sh
+++ install.sh
@@ -207,6 +207,7 @@ done
 # ─────────────────────────────────────
 echo "▶ Konfigurerar sudoers..."
 cat >/etc/sudoers.d/infomagic <<EOF
+$APP_USER ALL=(ALL) NOPASSWD:/sbin/reboot
 $APP_USER ALL=(ALL) NOPASSWD:/usr/bin/cec-client
 $APP_USER ALL=(ALL) NOPASSWD:/usr/bin/tee
 $APP_USER ALL=(ALL) NOPASSWD:/usr/bin/xhost
